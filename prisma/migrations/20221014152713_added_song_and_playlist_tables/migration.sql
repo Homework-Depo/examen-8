25ab999f86eb797553401010a111ad67405d2e33
javier-aponte <31341776+javier-aponte@users.noreply.github.com>
2022-10-14 10:32:10 -0500

    song and playlist tables added + small fix

---
+++ prisma/migrations/20221014152713_added_song_and_playlist_tables/migration.sql
@@ -0,0 +1,42 @@
+-- CreateTable
+CREATE TABLE "Song" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "artist" TEXT NOT NULL,
+    "album" TEXT NOT NULL,
+    "year" INTEGER NOT NULL,
+    "genre" TEXT NOT NULL,
+    "duration" INTEGER NOT NULL,
+
+    CONSTRAINT "Song_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Playlist" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "authorId" INTEGER NOT NULL,
+
+    CONSTRAINT "Playlist_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_PlaylistToSong" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_PlaylistToSong_AB_unique" ON "_PlaylistToSong"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_PlaylistToSong_B_index" ON "_PlaylistToSong"("B");
+
+-- AddForeignKey
+ALTER TABLE "Playlist" ADD CONSTRAINT "Playlist_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_PlaylistToSong" ADD CONSTRAINT "_PlaylistToSong_A_fkey" FOREIGN KEY ("A") REFERENCES "Playlist"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_PlaylistToSong" ADD CONSTRAINT "_PlaylistToSong_B_fkey" FOREIGN KEY ("B") REFERENCES "Song"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
 import express from "express";
-import cors from "cors";
 import { routes } from "./router";
 
 export const app = express();
 
-app.use(cors());
 app.use(express.json());
 routes(app);
